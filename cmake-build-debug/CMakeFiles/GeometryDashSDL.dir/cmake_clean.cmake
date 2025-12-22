4a3af685e43fc09ef3fe948e5024ce9e115c61b0
𝙰𝚞𝚐𝚞𝚜𝚝𝚒𝚗 <xaugustinx12@gmail.com>
2025-12-22 20:08:50 +0100

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/GeometryDashSDL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GeometryDashSDL.dir/link.d"
+  "CMakeFiles/GeometryDashSDL.dir/main.c.o"
+  "CMakeFiles/GeometryDashSDL.dir/main.c.o.d"
+  "GeometryDashSDL"
+  "GeometryDashSDL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GeometryDashSDL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
